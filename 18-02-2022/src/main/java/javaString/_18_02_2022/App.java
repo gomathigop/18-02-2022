de6b67af843af75ea183387f94ef2900af3c5b6e
GOMATHI KAVI <GOMATHI KAVI@DESKTOP-AB6DJ25>
2022-03-27 03:32:11 -0700

    GitHub

---
+++ 18-02-2022/src/main/java/javaString/_18_02_2022/App.java
@@ -0,0 +1,13 @@
+package javaString._18_02_2022;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ 18-02-2022/src/main/java/javaString/_18_02_2022/Array.java
@@ -0,0 +1,23 @@
+package javaString._18_02_2022;
+
+public class Array {
+
+	public static void main(String[] args) {
+		int marks[];
+	    marks=new int[5];
+	    marks[0]=80;
+	    marks[1]=100;
+	    marks[2]=100;
+	    marks[3]=70;
+	    marks[4]=90;
+	    
+	    System.out.println(marks[3]);
+	    System.out.println(marks[1]);
+	    System.out.println(marks[2]);
+	    
+		
+		
+
+	}
+
+}
